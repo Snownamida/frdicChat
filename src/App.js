a26729da03087dc8f007c340815e7b940faec5a6
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2024-04-25 20:27:22 +0200

    now okay to send and show messages......

--- src/App.js
+++ src/App.js
@@ -1,43 +1,83 @@
 import React, { useEffect, useState } from "react";
-import { MessageList } from "react-chat-elements";
+import { Button, Input, MessageList } from "react-chat-elements";
 import "react-chat-elements/dist/main.css";
 import ChatService from "./services/chatService";
 
 export default function App() {
   const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
   useEffect(() => {
     const getMessagesInterval = setInterval(async () => {
       try {
         const messages = await ChatService.getMessages();
-        console.log(messages);
-        setMessages(messages);
+        setMessages(messages.data);
       } catch (error) {
         console.error(error);
       }
-    }, 2000);
+    }, 5000);
     return () => {
       clearInterval(getMessagesInterval);
     };
   }, []);
+
+  const sendMessageHandler = async () => {
+    if (!message) return;
+    try {
+      const response = await ChatService.sendMessage(message);
+      if (response.message.startsWith("单词导入成功")) setMessage("");
+      else throw new Error(response.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <MessageList
-      className="message-list"
-      lockable={true}
-      toBottomHeight={"100%"}
-      dataSource={[
-        {
-          position: "left",
-          type: "text",
-          title: "Kursat",
-          text: "Give me a message list example !",
-        },
-        {
-          position: "right",
-          type: "text",
-          title: "Emre",
-          text: "That's all.",
-        },
-      ]}
-    />
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <MessageList
+        className="message-list"
+        lockable={true}
+        toBottomHeight={"100%"}
+        dataSource={messages
+          .map((massage) => JSON.parse(massage.word))
+          .map((message) => ({
+            position: "left",
+            type: "text",
+            title: message.ip,
+            text: message.text,
+          }))}
+      />
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          width: "100%",
+        }}
+      >
+        <div style={{ flexDirection: "row", width: "100%" }}>
+          <Input
+            style={{
+              width: "70%",
+            }}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="请输入"
+          />
+          <Button
+            style={{
+              width: "20%",
+              marginLeft: "auto",
+            }}
+            text="发送"
+            onClick={sendMessageHandler}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class ChatService {
   static async getMessages() {
     try {
-      const reponse = await axios.get("studylist/words/0", {
+      const reponse = await axios.get("/studylist/words/0", {
         headers: {
           Authorization:
             "NIS fIXVeb9bLQnDVHhji00UfRU1CQ0unDx106Nfjg/FHtHnT4NUJztKAg==",
@@ -13,6 +13,42 @@ export default class ChatService {
           language: "fr",
         },
       });
+      return reponse.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  static async sendMessage(message: string) {
+    let ip: string = "";
+    try {
+      const reponse = await axios.get("https://api.ipify.org?format=json");
+      ip = reponse.data.ip;
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      const reponse = await axios.post(
+        "/studylist/words",
+        {
+          id: "0",
+          language: "fr",
+          words: [
+            JSON.stringify({
+              ip: ip ? ip : "未知IP",
+              dateString: new Date().toISOString(),
+              text: message,
+            }),
+          ],
+        },
+        {
+          headers: {
+            Authorization:
+              "NIS fIXVeb9bLQnDVHhji00UfRU1CQ0unDx106Nfjg/FHtHnT4NUJztKAg==",
+          },
+          baseURL: "https://api.frdic.com/api/open/v1/",
+        }
+      );
       return reponse.data;
     } catch (error) {
       throw error;
